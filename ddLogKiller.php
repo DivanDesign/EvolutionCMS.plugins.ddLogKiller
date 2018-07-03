f60181ea85c8a61a166456933317e8cdead7f029
lavrov <lavrov@divandesign.ru>
2018-07-03 12:49:58 +0300

    Initial commit.

---
+++ ddLogKiller.php
@@ -0,0 +1,45 @@
+//<?php
+/**
+ * ddLogKiller
+ * @version 1.1 (2018-07-01)
+ * 
+ * @desc Плагин очищает лог событий, сохраняя минимальное заданное количество. Срабатывает раз в сессию каждого пользователя.
+ * 
+ * @param $rowsNumberToSave {integer} — Сколько записей лога сохранить. Default: 50.
+ * 
+ * @event OnWebPageInit
+ * 
+ * @copyright 2018 DivanDesign {@link http://www.DivanDesign.biz }
+ */
+
+//Run only 1 time per session
+if (!isset($_SESSION['ddLogKiller'])){
+	//Defaults
+	$rowsNumberToSave = isset($rowsNumberToSave) ? intval($rowsNumberToSave) : 50;
+	
+	$logsTableName = $modx->getFullTableName('event_log');
+	
+	//Total log rows
+	$rowsTotal = intval($modx->db->getValue($modx->db->select(
+		'COUNT(id)',
+		$logsTableName,
+		'type != 3'
+	)));
+	
+	if ($rowsTotal > $rowsNumberToSave){
+		$modx->db->delete(
+			//FROM
+			$logsTableName,
+			//WHERE
+			'type != 3',
+			//ORDER BY
+			'createdon DESC',
+			//LIMIT
+			$rowsTotal - $rowsNumberToSave
+		);
+	}
+	
+	//Latch
+	$_SESSION['ddLogKiller'] = true;
+}
+//?>
